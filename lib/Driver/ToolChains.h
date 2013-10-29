25242a3b3c5897962077b9d7d6e73d15812e0c94
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-10-29 12:52:45 +0000

    On FreeBSD 10, default to libc++.
    FreeBSD 10 removes libstdc++ on Clang architectures. Use libc++ instead.

--- lib/Driver/ToolChains.h
+++ lib/Driver/ToolChains.h
@@ -495,8 +495,13 @@ public:
   FreeBSD(const Driver &D, const llvm::Triple &Triple,
           const llvm::opt::ArgList &Args);
 
+  virtual CXXStdlibType GetCXXStdlibType(const llvm::opt::ArgList &Args) const;
+
   virtual bool IsMathErrnoDefault() const { return false; }
   virtual bool IsObjCNonFragileABIDefault() const { return true; }
+
+  virtual void AddClangCXXStdlibIncludeArgs(const llvm::opt::ArgList&,
+                                            tools::ArgStringList&) const;
 
   virtual bool UseSjLjExceptions() const;
 protected:
